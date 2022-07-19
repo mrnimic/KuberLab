b588a752b0fda073aa27fb1794d5acbd83e2edd6
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2022-07-19 17:12:32 +0430

    Automating SSH key generating and permitable source IP

--- wand.sh
+++ wand.sh
@@ -21,17 +21,22 @@ fi
 
 echo 'Please enter your Stack name:'
 read STACKNAME
+MYIP=$(curl icanhazip.com)
+echo "Your public IP address is $MYIP . This address would being used as the only permitted source IP for SSH."
+ssh-keygen -b 2048 -t rsa -f ./awskey -q -N ""
+SSHKEY=$(cat ./awskey.pub)
+echo "New SSH key has been created. This key would being used as your ssh key to login instances."
 
 STACKSTATUS=$(aws cloudformation list-stacks --query "StackSummaries[?StackName == '${STACKNAME}'].StackStatus | [0]")
 
 if [ "$STACKSTATUS" == "null" ] || [ "$STACKSTATUS" == "CREATE_FAILED"]; then
   echo "This is a new Stack. Let's create it ... "
   echo 'Waiting to create Stack ...'
-  aws cloudformation create-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml && aws cloudformation wait stack-create-complete --stack-name $STACKNAME
+  aws cloudformation create-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue=$SSHKEY ParameterKey=SshSourceIp,ParameterValue=$MYIP && aws cloudformation wait stack-create-complete --stack-name $STACKNAME
 elif [ "$STACKSTATUS" == "UPDATE_ROLLBACK_COMPLETE" ] || [ "$STACKSTATUS" == "CREATE_COMPLETE" ] || [ "$STACKSTATUS" == "UPDATE_COMPLETE" ] || [ "$STACKSTATUS" == "UPDATE_FAILED" ]; then
   echo "This Stack has already been deployed. Let's update it ... "
   echo 'Waiting to update Stack ...'
-  aws cloudformation update-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml && aws cloudformation wait stack-update-complete --stack-name $STACKNAME
+  aws cloudformation update-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue=$SSHKEY ParameterKey=SshSourceIp,ParameterValue=$MYIP && aws cloudformation wait stack-update-complete --stack-name $STACKNAME
 else
   echo "Stack status is $STACKSTATUS"
   exit 1
