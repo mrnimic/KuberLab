611735481a813691de58d78b1088c889f7bc1b00
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2022-07-20 13:15:18 +0430

    Changing the source IP definition

--- wand.sh
+++ wand.sh
@@ -34,11 +34,11 @@ STACKSTATUS=$(aws cloudformation list-stacks --query "StackSummaries[?StackName 
 if [[ "$STACKSTATUS" == "null" ]] || [[ "$STACKSTATUS" == '"CREATE_FAILED"' ]] || [[ "$STACKSTATUS" == '"DELETE_COMPLETE"' ]]; then
   echo "This is a new Stack. Let's create it ... "
   echo 'Waiting to create Stack ...'
-  aws cloudformation create-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue="$SSHKEY" ParameterKey=SshSourceIp,ParameterValue="$MYIP" && aws cloudformation wait stack-create-complete --stack-name $STACKNAME
+  aws cloudformation create-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue="$SSHKEY" ParameterKey=SshSourceIp,ParameterValue="$MYIP/32" && aws cloudformation wait stack-create-complete --stack-name $STACKNAME
 elif [[ "$STACKSTATUS" == '"UPDATE_ROLLBACK_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"CREATE_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"UPDATE_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"UPDATE_FAILED"' ]] || [[ "$STACKSTATUS" == '"ROLLBACK_COMPLETE"' ]]; then
   echo "This Stack has already been deployed. Let's update it ... "
   echo 'Waiting to update Stack ...'
-  aws cloudformation update-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue="$SSHKEY" ParameterKey=SshSourceIp,ParameterValue="$MYIP" && aws cloudformation wait stack-update-complete --stack-name $STACKNAME
+  aws cloudformation update-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue="$SSHKEY" ParameterKey=SshSourceIp,ParameterValue="$MYIP/32" && aws cloudformation wait stack-update-complete --stack-name $STACKNAME
 else
   echo Stack status is "$STACKSTATUS"
   exit 1
