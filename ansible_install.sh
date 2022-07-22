f649b4cd6193e7161d4d8fb0ed099d283a4b2e3e
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2022-07-22 20:53:44 +0430

    Disabling host_key_checking in /etc/ansible/ansible.cfg

--- ansible_install.sh
+++ ansible_install.sh
@@ -2,6 +2,7 @@
 
 sudo apt update && sudo apt install -y ansible
 sudo touch /etc/ansible/hosts
+sudo sed -i 's/^#host_key_checking.*/host_key_checking = False/g' /etc/ansible/ansible.cfg
 
 echo "> Testing connection ..."
 ansible all -m ping -i ./AnsibleInventory -u ubuntu --private-key ./awskey
