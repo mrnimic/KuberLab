a27a0f34a385899cab473c3258a292cc2334cfaf
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2022-07-23 09:56:06 +0430

    Fixing update Stack bugs

--- wand.sh
+++ wand.sh
@@ -2,7 +2,6 @@
 
 chmod +x ./ansible_install.sh
 rm -f ./AnsibleInventory
-rm -f ./awskey*
 HOMEDIR=$(echo $HOME)
 
 #Installing awscli V2 on this machine
@@ -40,7 +39,6 @@ echo '>Please enter your Stack name:'
 read STACKNAME
 MYIP=$(curl -s icanhazip.com)
 echo ">Your public IP address is $MYIP . This address would being used as the only permitted source IP for SSH."
-ssh-keygen -b 2048 -t rsa -f ./awskey -q -N ""
 SSHKEY=$(cat ./awskey.pub)
 echo ">New SSH key has been created. This key would being used as your ssh key to login instances."
 IPLEN=$(curl -s icanhazip.com | wc -c | tr -d ' ')
@@ -54,12 +52,14 @@ fi
 
 if [[ "$STACKSTATUS" == "null" ]] || [[ "$STACKSTATUS" == '"CREATE_FAILED"' ]] || [[ "$STACKSTATUS" == '"DELETE_COMPLETE"' ]]; then
   echo ">This is a new Stack. Let's create it ... "
-  echo '>Waiting to create Stack ...'
   if [ "$IPLEN" -gt 17 ];then
     echo ">Your IP address is IPV6. Please change your IP address or internet provider."
     exit 1
   else
-    echo ">Your IP address is IPV4. Resuming the script..."
+    echo ">Your IP address is IPV4. Waiting to create Stack ..."
+    rm -f ./awskey*
+    ssh-keygen -b 2048 -t rsa -f ./awskey -q -N ""
+    SSHKEY=$(cat ./awskey.pub)
     aws cloudformation create-stack --stack-name $STACKNAME --template-body file://EC2Instance.yml --parameters ParameterKey=SshKeyPair,ParameterValue="$SSHKEY" ParameterKey=SshSourceIpV4,ParameterValue="$MYIP/32" && aws cloudformation wait stack-create-complete --stack-name $STACKNAME
   fi
 elif [[ "$STACKSTATUS" == '"UPDATE_ROLLBACK_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"CREATE_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"UPDATE_COMPLETE"' ]] || [[ "$STACKSTATUS" == '"UPDATE_FAILED"' ]]; then
@@ -83,6 +83,7 @@ aws cloudformation describe-stacks --stack-name $STACKNAME --query "Stacks[0].Ou
 echo "[k8sWorkers]" >> ./AnsibleInventory
 aws cloudformation describe-stacks --stack-name $STACKNAME --query "Stacks[0].Outputs[?OutputKey == 'Worker1PublicIP'].OutputValue" --output text >> ./AnsibleInventory
 aws cloudformation describe-stacks --stack-name $STACKNAME --query "Stacks[0].Outputs[?OutputKey == 'Worker2PublicIP'].OutputValue" --output text >> ./AnsibleInventory
+
 echo ">installing Ansible ..."
 sh ./ansible_install.sh
 
