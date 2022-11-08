bdc2212415d4586bd214b670f08dc219835aa286
mrnimic <n.amiri69@gmail.com>
2022-11-08 14:39:28 +0000

    Edit .gitignore - Change method

--- main.tf
+++ main.tf
@@ -1,18 +1,142 @@
-locals {
-  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
-  region = "eu-west-1"
-
-  tags = {
-    Example    = local.name
-    GithubRepo = "terraform-aws-eks"
-    GithubOrg  = "terraform-aws-modules"
-  }
+resource "aws_vpc" "nimic-vpc" {
+  cidr_block = "10.10.0.0/16"
+  enable_dns_support = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name 
+}
+resource "aws_subnet" "nimic-subnet-1" {
+    vpc_id = "${aws_vpc.nimic-vpc.id}"
+    cidr_block = "10.10.10.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "nimic-subnet-2" {
+    vpc_id = "${aws_vpc.nimic-vpc.id}"
+    cidr_block = "10.10.20.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "us-east-1b"
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.30.3"
-  cluster_name = local.name
-  
-}
+  version = "~> 18.0"
 
+  cluster_name    = "my-cluster"
+  cluster_version = "1.22"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  vpc_id     = aws_vpc.nimic-vpc.id
+  subnet_ids = [aws_subnet.nimic-subnet-1.id, aws_subnet.nimic-subnet-2.id]
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "t3.medium"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    ]
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "m6i.large"
+            weighted_capacity = "2"
+          },
+        ]
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["t3.medium", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
