14fa26e6d98f4835566c9150b9b1a1b709cfb79d
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2023-04-30 20:35:02 +0330

    evening

--- tf-eks.tf
+++ tf-eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.5.1"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = "kuberlab-eks-cluster"
   cluster_version = "1.24"
 
   vpc_id                         = module.vpc.vpc_id
