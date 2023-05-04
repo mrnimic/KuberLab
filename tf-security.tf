0a3edc43658396b7122ae3b91cbb701aef0f139c
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2023-05-05 00:16:54 +0330

    Add Cluster SG

--- tf-security.tf
+++ tf-security.tf
@@ -24,3 +24,20 @@ resource "aws_security_group" "kuberlab-sg-ec2" {
         cidr_blocks = ["10.0.0.0/8"]
     }
 }
+
+resource "aws_security_group" "kuberlab-sg-cluster" {
+    vpc_id = "${module.vpc.vpc_id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["10.0.0.0/8"]
+    }
+}
