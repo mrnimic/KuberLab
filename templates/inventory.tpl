6600eea20c2afe9ef2caa68d27bb5856df07ce70
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2023-05-05 00:12:35 +0330

    Add Cluster Name to Inventory

--- templates/inventory.tpl
+++ templates/inventory.tpl
@@ -2,3 +2,7 @@
 masternode ansible_host=%{ for ip in master ~}
 ${ip}
 %{ endfor ~}
+
+[masters:vars]
+cluster_name=${cluster_name}
+
--- tf-ansible_inventory_creator.tf
+++ tf-ansible_inventory_creator.tf
@@ -2,6 +2,7 @@ resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/templates/inventory.tpl",
     {
       master = aws_instance.controlPlane.*.public_ip
+      cluster_name = module.eks.cluster_name
     }
   )
   filename = "./ansible/hosts.cfg"
