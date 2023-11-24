32df1debf3fd47af7d429e509094ba40040144f8
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2023-11-24 13:37:01 +0330

    Increase Bastion's Storage to 50G

--- Terraform/tf-instances.tf
+++ Terraform/tf-instances.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "controlPlane" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
   root_block_device {
-    volume_size = 20
+    volume_size = 50
   }
   subnet_id = "${element(module.vpc.public_subnets, 0)}"
   vpc_security_group_ids = ["${aws_security_group.kuberlab-sg-ec2.id}"]
