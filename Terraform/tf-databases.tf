4014068c14725f2a260fa532c5fe4bcacb43b53f
Nima Amiri <nima@NimaAmiris-MacBook-Pro.local>
2023-12-30 12:48:55 +0330

    Ending Project

---
+++ Terraform/tf-databases.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "bsb-dynamodb-table" {
+  name           = "bsb-dynamodb-table"
+  hash_key       = "PK"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "bsb-dynamodb-table"
+  }
+}
